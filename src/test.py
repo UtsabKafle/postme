93e6b29536f13872d3d3837e59a0dd87db7be3f8
utsab kafle <utsabcodes@gmail.com>
2022-09-24 16:28:43 +0545

    something

--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 from email import header
+from turtle import xcor
 from PIL import Image, ImageDraw, ImageFont
 import os
 
@@ -18,6 +19,9 @@ class post:
                 'stories':(1080,1920),
                 'square':(1080,1080)
                 }
+        }
+        self.positions = {
+            
         }
         self.font_size = {
             'watermark':20,
@@ -77,6 +81,7 @@ class post:
     def draw(self,size=(1024,720),color=(0,0,0),):
         self.jii = Image.new('RGB',size,color=color)
         self.wr = ImageDraw.Draw(self.jii)
+        print(size)
 
     def choose_pos(self):
         x_cor = int(input("enter the x cordinate"))
@@ -86,40 +91,68 @@ class post:
         self.wr.text(position,text,fill=fill_,font=font_)
     def draw_header(self):
         self.wr.text((512,20),"your sweet heading",fill=(255,255,255),font=self.font)
-
     def draw_content(self):
         self.wr.text((20,200),"They just .. ladfjl",fill=(255,255,255))
     def show(self):
         self.jii.show()
     def save(self,file_name):
         self.jii.save(file_name)
+    def draw_logo(self):
+        x_corr = 895
+        y_corr = 1000
+        logo_f_size = 15
+        logo_font = self.fonts_['LibreFranklin-SemiBold']
+        logo_font = self.select_font(logo_font,logo_f_size)
+        self.wr.text((x_corr,y_corr),"@geekbind",font=logo_font)
+    
+    def draw_title(self):
+        title = "How do computers read binary?"
+        x_corr,y_corr = self.size
+        x_corr = 305
+        y_corr = 50
+        print(x_corr)
+        tits_f_size = 45
+        tits_font = self.fonts_['Roboto-Medium']
+        tits_font = self.select_font(tits_font,tits_f_size)
+        self.wr.text((x_corr,y_corr),title,font=tits_font)
 
+
+
+        
+        
 def main():
+    logo_pos = ()
+    title_pos = ()
+    content_pos = ()
+
     p = post()
     mode = p.choose_mode()
-    logo_font = p.choose_font()
-    position_ = p.choose_pos()
-    background_color = p.choose_color()
+    # logo_font = p.choose_font()
+    # position_ = p.choose_pos()
+    # background_color = p.choose_color()
+    background_color = (143, 0, 179)
     p.draw(size=mode,color=background_color)
-    logo_f_size = p.choose_font_size()
-    logo_font = p.select_font(logo_font,logo_f_size)
-    p.draw_wm(text="@shittypage69",position=position_,font_=logo_font)
-    # p.draw_wm(text="@crap",font_=None)
-    print('logo end -- header')
+    p.draw_logo()
+    p.draw_title()
+    # logo_f_size = p.choose_font_size()
+    # logo_font = p.select_font(logo_font,logo_f_size)
+    # p.draw_wm(text="@shittypage69",position=position_,font_=logo_font)
+    # # p.draw_wm(text="@crap",font_=None)
+    # print('logo end -- header')
     # header_font = p.choose_font()
     # header_f_size = p.choose_font_size()
     # header_font = p.select_font(header_font,header_f_size)
     # position_ = p.choose_pos()
-    p.draw_wm(text="How do computer read 0 and 1?",font_=None)
+    # p.draw_wm(text="How do computer read 0 and 1?",font_=header_font,position=position_)
     p.show()
-    print('header end -- content')
-    cont_font = p.choose_font()
-    cont_f_size = p.choose_font_size()
-    position_ = p.choose_pos()
-    cont_font = p.select_font(cont_font,cont_f_size)
-    p.draw_wm(text="This is a long process which is carried out by different method and is very good and shitty at the same time, haha watever but. fuck shit crap piss blah blah whatever",position=position_,font=cont_font,)
-    p.show()
-    p.save('crap.png')
+    # print('header end -- content')
+    # cont_font = p.choose_font()
+    # cont_f_size = p.choose_font_size()
+    # position_ = p.choose_pos()
+    # cont_font = p.select_font(cont_font,cont_f_size)
+    # p.draw_wm(text="This is a long process which is carried out by different method and is very good and shitty at the same time, haha watever but. fuck shit crap piss blah blah whatever",position=position_,font_=cont_font,)
+    # p.show()
+    p.save("ddd.png")
 
 
 if __name__ == "__main__":

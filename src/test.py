69926410f2bde501d72dc150a17da7da7132b53c
utsab kafle <utsabcodes@gmail.com>
2022-09-24 20:18:03 +0545

    nearly done

--- src/test.py
+++ src/test.py
@@ -100,40 +100,100 @@ class post:
     def draw_logo(self):
         x_corr = 895
         y_corr = 1000
-        logo_f_size = 15
+        logo_f_size = 25
         logo_font = self.fonts_['LibreFranklin-SemiBold']
         logo_font = self.select_font(logo_font,logo_f_size)
         self.wr.text((x_corr,y_corr),"@geekbind",font=logo_font)
     
     def draw_title(self):
         title = "How do computers read binary?"
-        x_corr,y_corr = self.size
         x_corr = 305
-        y_corr = 50
+        y_corr = 70
         print(x_corr)
         tits_f_size = 45
         tits_font = self.fonts_['Roboto-Medium']
         tits_font = self.select_font(tits_font,tits_f_size)
         self.wr.text((x_corr,y_corr),title,font=tits_font)
 
+    def draw_content(self,text="",type='content'):
+        if type == 'title':
+            x_corr = 305
+            y_corr = 70
+            tits_f_size = 45
+            tits_font = self.fonts_['Roboto-Medium']
+            tits_font = self.select_font(tits_font,tits_f_size)
+            self.wr.text((x_corr,y_corr),text,font=tits_font)
+        elif type == 'logo':
+            x_corr = 895
+            y_corr = 1000
+            logo_f_size = 25
+            logo_font = self.fonts_['LibreFranklin-SemiBold']
+            logo_font = self.select_font(logo_font,logo_f_size)
+            self.wr.text((x_corr,y_corr),text,font=logo_font)
+        else:
+            title = """
+                This is a very long process. It takes alot of 
+                effort to achieve.
+
+                I have no idea what I am writting. I think this is 
+                enough to see the results
+                but not that good for usl.
+            """
+            x_corr = 10
+            y_corr = 150
+            print(x_corr)
+            tits_f_size = 30
+            tits_font = self.fonts_['NotoSansGeorgian-SemiBold']
+            tits_font = self.select_font(tits_font,tits_f_size)
+            self.wr.text((x_corr,y_corr),text,font=tits_font)
+    
 
 
-        
-        
+
+
 def main():
     logo_pos = ()
     title_pos = ()
     content_pos = ()
 
     p = post()
-    mode = p.choose_mode()
+    # mode = p.choose_mode()
+    mode = (1080,1080)
+
     # logo_font = p.choose_font()
     # position_ = p.choose_pos()
     # background_color = p.choose_color()
-    background_color = (143, 0, 179)
+    cont = """
+    We all have read or heard that computers use binary number system. But why? Why not decimal?
+
+First of all, computers use billions of transistors for storing the bits. Transistor made-up of a semi-conductor material, meaning it can act as an insulator as well as an conductor; depending on the voltage applied. When it is acting as an insulator, i.e. current is absent, it is considered a 0 bit. Similarly, when it is acting as a conductor, it is considered a 1 bit.
+
+So clearly, there are only two possibilities for a single transistor to work on. So, binary.
+
+ But still decimals can be used with it, but we always use binary. Let's see the advantages of using binary over decimal.
+
+      suppose I want to add two numbers 5 and 32 using in computer. Now, using decimal system; I will first turn on 5 transistors in a sequence to represent 5 and other 22 transistors to represents the number 32. Here I had to use 27 transistors.
+   
+     1.1.1.1.1 + 1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1
+     
+     Let's redo this same activity using binary. In binary, 5 means 101. So I will make a sequence of 3 transistors and turn on the first and the last one of the sequence. Now 22 is 10110 in binary. So I will make another sequence of 5 transistors and turn on the first, third and the forth ones. Here, I used only 8 transistors.
+
+     1.0.1 + 1.0.1.1.0
+
+You can see the difference in the number of transistors used. 27 is way more than 8. That's why we use binary.
+    """
+    words = cont.split(" ")
+    for i in range(1,len(words)+1):
+        if i%10==0:
+            words[i] += "\n"
+    cont_ = ""
+    for i in range(len(words)):
+        cont_ += words[i]+" "
+    background_color = (63, 70, 159)
     p.draw(size=mode,color=background_color)
-    p.draw_logo()
-    p.draw_title()
+    p.draw_content(text="@geekbind",type='logo')
+    p.draw_content(text="Why do computers use binary?",type='title')
+    p.draw_content(text=cont_)
     # logo_f_size = p.choose_font_size()
     # logo_font = p.select_font(logo_font,logo_f_size)
     # p.draw_wm(text="@shittypage69",position=position_,font_=logo_font)
